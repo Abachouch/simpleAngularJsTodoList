79468740fa2e6ac71411287f0ba1f4cbbb245a50
A.Youssouf <youssouf.abachouch@gmail.com>
2017-12-29 23:14:27 +0000

    create form to add new todo

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,15 +1,21 @@
 app.controller('manageTodo', function($scope) {
     $scope.firstName = "John";
     $scope.lastName = about();
-    $scope.allTodo = getAllTodo();
+    $scope.allTodo = getAllTodo().reverse();
     $scope.$on('removeTask' , function(){
-      $scope.allTodo = getAllTodo();
-    })
+      $scope.allTodo = getAllTodo().reverse();
+    });
     
-    
+    $scope.saveTodo = function() {
+      if(this.newTask) {
+        addTodo($scope.newTask) ;
+        console.log('adding : '+$scope.newTask) ;
+        $scope.allTodo = getAllTodo().reverse();
+      }
+    };
+
     function hp(){
       console.log('hp') ;
     }
-    
     
   });
