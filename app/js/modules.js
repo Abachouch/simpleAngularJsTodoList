89e33c169ddc4dafa9efa0eb5eb48df248f77195
A.Youssouf <youssouf.abachouch@gmail.com>
2018-01-02 14:31:53 +0000

    fix rooter links

--- app/js/modules.js
+++ app/js/modules.js
@@ -5,8 +5,24 @@ app.config(function($routeProvider) {
         templateUrl : "app/pages/todo.html",
         controller : "todoCtrl"
     })
+    .when("/simpleAngularJsTodoList/", {
+        templateUrl : "app/pages/todo.html",
+        controller : "todoCtrl"
+    })
     .when("/done", {
         templateUrl : "app/pages/done.html",
+        controller : "doneCtrl"
+    })
+    .when("/simpleAngularJsTodoList/done", {
+        templateUrl : "app/pages/done.html",
+        controller : "doneCtrl"
+    })
+    .when("/simpleAngularJsTodoList/pomodoro", {
+        templateUrl : "app/pages/404.html",
+        controller : "doneCtrl"
+    })
+    .when("/simpleAngularJsTodoList/statistics", {
+        templateUrl : "app/pages/404.html",
         controller : "doneCtrl"
     })
     
